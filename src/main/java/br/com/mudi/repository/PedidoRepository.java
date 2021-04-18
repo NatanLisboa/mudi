c4d5d109e4e95c68344da3afc7b2ba8107f12f79
unknown <natanflisboa1@gmail.com>
2021-04-17 22:12:01 -0300

    Adiciona Spring Data JPA

--- src/main/java/br/com/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/mudi/repository/PedidoRepository.java
@@ -1,27 +1,11 @@
 package br.com.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
-
-	@PersistenceContext
-	private EntityManager entityManager;
-	
-	@SuppressWarnings("unchecked")
-	public List<Pedido> recuperarPedidos() {
-		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		return query.getResultList();		
-		
-	}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 	
 }
